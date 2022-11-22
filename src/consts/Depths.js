b1cf5d60ac01a68672d4c357d55aec4898341966
Umz <umz@umzgames.com>
2022-11-22 23:18:43 +0000

    Created Depths- file to hold ALL depths of objects

---
+++ src/consts/Depths.js
@@ -0,0 +1,18 @@
+const Depths = {
+
+    BG_SCENE: 1,
+
+    ENEMIES: 20,
+    ENEMIES_FROZEN: 14,
+    ENEMIES_SPAWNED: 9,
+
+    PLAYERS: 15,
+
+    BUILDINGS_BG: 10,
+    BUILDINGS_FG: 25,
+
+    FOREST_BG1: 10,
+    FOREST_BG2: 11,
+    FOREST_FG1: 25
+}
+export default Depths;

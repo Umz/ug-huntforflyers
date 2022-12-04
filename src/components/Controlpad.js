84f84f1ee02020362eb2a553fa6631262ed5790a
Umz <umz@umzgames.com>
2022-12-04 14:39:36 +0000

    Added controls for WASD

--- src/components/Controlpad.js
+++ src/components/Controlpad.js
@@ -18,6 +18,12 @@ class Controlpad {
         scene.input.keyboard.on('keydown-RIGHT', (event) => { this.RIGHT = true });
         scene.input.keyboard.on('keyup-RIGHT', (event) => { this.RIGHT = false });
         scene.input.keyboard.on('keydown-Z', (event) => { this.action() });
+
+        scene.input.keyboard.on('keydown-A', (event) => { this.LEFT = true; });
+        scene.input.keyboard.on('keyup-A', (event) => { this.LEFT = false });
+        scene.input.keyboard.on('keydown-D', (event) => { this.RIGHT = true });
+        scene.input.keyboard.on('keyup-D', (event) => { this.RIGHT = false });
+        scene.input.keyboard.on('keydown-S', (event) => { this.action() });
     }
 
     action() {

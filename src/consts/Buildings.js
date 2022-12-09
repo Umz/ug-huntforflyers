2d0bf093c4cffe330888bb79ffc919328c0e90a5
Umz <umz@umzgames.com>
2022-12-09 22:44:12 +0000

    Added more buildings-

--- src/consts/Buildings.js
+++ src/consts/Buildings.js
@@ -3,5 +3,14 @@ const Buildings = {
     LAB_TABLE: 'decor_lab_table',
     PLAYER_HOUSE: 'building_main',
     WATER_PUMP: 'building_pump',
+
+    TENT1: "building_tent1",
+    TENT2: "building_tent2",
+    TENT3: "building_tent3",
+
+    HUT: "building_hut",
+    HOUSE1: "building_house1",
+
+    WINDMILL: "building_windmill"
 }
 export default Buildings;
---
+++ src/consts/Decor.js
@@ -0,0 +1,7 @@
+const Decor = {
+
+    BARREL: "decor_barrel",
+    CHEST: "decor_chest",
+    SIGN: "decor_sign1",
+}
+export default Decor;

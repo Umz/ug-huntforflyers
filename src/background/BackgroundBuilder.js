bed2478685814346419d310302d70e2ee644d39e
Umz <umz@umzgames.com>
2022-12-17 21:19:51 +0000

    Applying new depths to background layers

--- src/background/BackgroundBuilder.js
+++ src/background/BackgroundBuilder.js
@@ -1,3 +1,4 @@
+import Depths from "../consts/Depths";
 import WorldConsts from "../consts/WorldConsts";
 import TextureMapper from "../mappers/TextureMapper";
 
@@ -7,6 +8,7 @@ class BackgroundBuilder {
         for (let i=0; i<6; i++) {
 
             let bg = scene.add.tileSprite(0, 0, WorldConsts.WIDTH, WorldConsts.HEIGHT, 'background', `bg${i}`).setOrigin(0).setScrollFactor(0);
+            bg.setDepth(Depths.BG_SCENE + i);
             bg.sf = (i * .15);
             bg.of = 0;
 
@@ -24,6 +26,7 @@ class BackgroundBuilder {
 
     static addGround(scene) {
         let gr = scene.add.tileSprite(0, WorldConsts.GROUND_Y, WorldConsts.WIDTH, 64, 'background', `ground_0`).setOrigin(0).setScrollFactor(0);
+        gr.setDepth(Depths.BG_STAGE);
         gr.update = function(time, delta) {
             let camera = this.scene.cameras.main;
             this.tilePositionX = camera.scrollX;
@@ -36,38 +39,6 @@ class BackgroundBuilder {
         let atlas = TextureMapper.getAtlas(building.type);
         let b = scene.add.sprite(building.worldX, WorldConsts.GROUND_Y, atlas, building.type).setOrigin(.5, 1).setDepth(building.depth).setAlpha(building.alpha);
         return b;
-    }
-
-    //  HOW to Split for Depths?
-    static addForestUnit(scene, startX, trees) {
-
-        //let forestLayer = {tint, alpha, depth, PlantTypes};
-        
-        //  Add Layer - addTrees(scene, layer);
-        BackgroundBuilder.addTrees(scene, startX + 45, trees - 2, 0x000000, .5);
-        BackgroundBuilder.addTrees(scene, startX + 15, trees - 2, 0x000000, .7);
-        BackgroundBuilder.addTrees(scene, startX, trees);
-
-        let gap = Phaser.Math.Between(10, 50);
-        for (let i=-1; i < trees + 1; i++) {
-            let frame = Phaser.Math.RND.pick(['decor_bush', 'flower1', 'flower2', 'flower3']);
-            scene.add.image(startX + (i * gap), WorldConsts.GROUND_Y, 'background', frame).setOrigin(.5, 1).setDepth(Phaser.Math.Between(0, 1));
-        }
-    }
-
-    static addTrees(scene, startX, amt, tint, alpha) {
-
-        let y = WorldConsts.GROUND_Y;
-        let gap = Phaser.Math.Between(30, 50);
-        
-        for (let i=0; i < amt; i++) {
-            let frame = Phaser.Math.RND.pick(['decor_tree1', 'decor_tree2']);
-            let flip = Math.random() > .5;
-            let depth = (tint === undefined) ? Phaser.Math.Between(0, 1) : 0;
-            let tree = scene.add.image(startX + (i * gap), y, 'background', frame).setOrigin(.5, 1).setFlipX(flip).setDepth(depth);
-            if (tint !== undefined)
-                tree.setTintFill(tint).setAlpha(alpha);
-        }
     }
 
     static addForest(scene, forest) {

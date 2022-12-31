4fe2d5403f30a91f6392e2bae414dc6c2a135993
Umz <umz@umzgames.com>
2022-12-31 10:18:06 +0000

    Default load 5

--- src/components/GameSave.js
+++ src/components/GameSave.js
@@ -6,7 +6,7 @@ class GameSave {
     static Init() {
         sessionStorage.setItem(Consts.SAVE_NAME, "-");
         sessionStorage.setItem(Consts.SAVE_NAME_SCORE, 0);
-        sessionStorage.setItem(Consts.SAVE_STAGE, Levels.STAGE2);
+        sessionStorage.setItem(Consts.SAVE_STAGE, Levels.STAGE5);
     }
 
     static SetScore(val) {

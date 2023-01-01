352936e1d7a693f99f894b5bba1e7892051e4d77
Umz <umz@umzgames.com>
2023-01-01 14:08:17 +0000

    Cleaned ctrl for thief enemy-

--- src/enemy/ThiefCtrl.js
+++ src/enemy/ThiefCtrl.js
@@ -2,20 +2,24 @@ import CtrEnemyFly from "../actions/CtrEnemyFly";
 import CtrFollowTarget from "../actions/CtrFollowTarget";
 import CtrListenFrozen from "../actions/CtrListenFrozen";
 import CtrMoveToTargetX from "../actions/CtrMoveToTargetX";
-import CtrMoveToX from "../actions/CtrMoveToX";
 import CtrSteal from "../actions/CtrSteal";
 import CtrStealDive from "../actions/CtrStealDive";
+import CtrWait from "../actions/CtrWait";
 import BaseController from "../classes/BaseController";
 import FnNames from "../consts/FnNames";
 import States from "../consts/States";
 
 class ThiefCtrl extends BaseController {
+
     constructor(target) {
         super(target);
         this.scene = this.sprite.scene;
-
-        this.followPlayer();
+        this.setDefaults();
+    }
+    
+    setDefaults() {
         this.flyNormal();
+        this.followPlayer();
         this.listenForFrozen();
     }
 
@@ -36,35 +40,35 @@ class ThiefCtrl extends BaseController {
     }
 
     moveToFrozen() {
+        this.target.removeUpdateFn(FnNames.ACT_FOLLOW_TARGET);
         let crystal = this.scene.getClosestFrozen(this.sprite);
-        this.addAction(new CtrMoveToTargetX(this.sprite, crystal).addCallback(()=>{this.dropToCollect(crystal)}));
-    }
-
-    dropToCollect(crystal) {
-        this.clearAllActions();
-        this.target.removeUpdateFn(FnNames.ACT_ENEMY_FLY);
-
-        this.addAction(new CtrStealDive(this.sprite, crystal).addCallback(()=>{
-            this.flyAwayWithSteal(crystal);
+        this.addAction(new CtrMoveToTargetX(this.sprite, crystal).addCallback(()=>{
+            this.dropToCollect(crystal);
         }));
     }
 
-    flyAwayWithSteal(crystal) {
-        if (crystal.parent.isStateEquals(States.FROZEN)) {
-            crystal.parent.setState(States.STOLEN);
-            this.addAction(new CtrSteal(this.sprite, crystal));
-            this.target.claimCrystal(crystal);
-            crystal.setCollideWorldBounds(false);
-            console.log('Successful steal')
-        }
-        else {
-            this.flyNormal();
-            this.followPlayer();
-            this.listenForFrozen();
-
-            console.log('Resetting to behaviour')
-        }
+    dropToCollect(crystal) {
+        this.target.removeUpdateFn(FnNames.ACT_ENEMY_FLY);
+        this.addAction(new CtrStealDive(this.sprite, crystal).addCallback(()=>{
+            this.attemptToSteal(crystal);
+        }));
     }
 
+    attemptToSteal(crystal) {
+        let prey = crystal.parent;
+        if (prey.isStateEquals(States.FROZEN)) {
+            prey.setState(States.STOLEN);
+            crystal.setCollideWorldBounds(false);
+            this.addAction(new CtrSteal(this.sprite, crystal).addCallback(()=>{
+                this.setDefaults();
+            }));
+        }
+        else {
+            this.sprite.setVelocityY(-40);
+            this.addAction(new CtrWait(500).addCallback(()=>{
+                this.setDefaults();
+            }));
+        }
+    }
 }
 export default ThiefCtrl;

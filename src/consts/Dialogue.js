8a50402734d9706e7a3cba5278d76d41b547fd71
Umz <umz@umzgames.com>
2023-02-11 14:27:50 +0000

    Civilians speaking according to house type

--- src/consts/Dialogue.js
+++ src/consts/Dialogue.js
@@ -1,7 +1,185 @@
 const Dialogue = {
 
     TYPE_PLAYER: "speech-main",
+    TYPE_CIV: "speech-civ",
 
     LEVEL_COMPLETE: "Great work. The Farborne have completed renovations. We can move to the next phase! Enter the lab.",
+
+    NPC: {
+        TENT1: [
+            [
+                "What?",
+                "What is it?",
+                "What do you want?",
+                "... What?",
+                "Why are you bothering me?",
+                "Yes, what? What?",
+                "..."
+            ],
+            [
+                "I need to eat.",
+                "What do I do now?",
+                "I need to build.",
+                "Should I stay here?",
+                "I need my space.",
+                "Do you need something?",
+                "What next?"
+            ],
+            [
+                "Such a small space...",
+                "What luck I have.",
+                "If I were born rich...",
+                "... If only...",
+                "Life in this place.",
+                "Are you here to help?",
+                "No one will help..."
+            ]
+        ],
+        TENT2: [
+            [
+                "As-Salaamu alaikum Professor.",
+                "How are you?",
+                "You should paint that vehicle of yours.",
+                "You look well. Very well today.",
+                "Those followers of yours...",
+                "Your followers look healthy.",
+                "Stay well and healthy Professor!"
+            ],
+            [
+                "Look at the sky today.",
+                "What a beautiful sky it is.",
+                "Alhamdu lillah!",
+                "Trees, soil, water, honey, meat.",
+                "This place is much better now!",
+                "I never imagined a place as this,",
+                "Surely, we are blessed with this."
+            ],
+            [
+                "Today is an opportunity.",
+                "We have opportunity we did not have yesterday.",
+                "Grasp the day!",
+                "We will achieve! Insha'Allah!",
+                "There is much to do today.",
+                "Keep active, keep strong, keep focused!",
+                "This opportunity will be good for us."
+            ]
+        ],
+        TENT3: [
+            [
+                "Alhamdu lillah, look around us.",
+                "This land was barren before.",
+                "Isn't this change so amazing.",
+                "Large tents, cool air, fresh water, vegetables.",
+                "It is like we are kings on this earth.",
+                "We have never lived so good before.",
+                "How amazing, subhanAllah!"
+            ],
+            [
+                "We didn't used to have all of this.",
+                "The land was never so green and full of life like this.",
+                "This is my first time with soil.",
+                "And my first time walking on grassland.",
+                "Alhamdu lillah look at me now.",
+                "We have clean water for all.",
+                "Your hands are blessed Professor!"
+            ],
+            [
+                "There is nothing else I want.",
+                "Allahu akbar! What a great life this is.",
+                "We live here amongst the trees and plants.",
+                "The scorching sands stay away.",
+                "Where else would I seek to be.",
+                "This place is like a paradise on earth.",
+                "May you be rewarded Professor."
+            ]
+        ],
+        HUT: [
+            [
+                "A wonderful place, ma shaa Allah.",
+                "What we need to build is a roadway.",
+                "Our pipework is good.",
+                "Perhaps we should increase our agriculture.",
+                "We have the water supply for farmland.",
+                "What do you think Professor?",
+                "How should we progress?"
+            ],
+            [
+                "Professor, how wonderful to see you out.",
+                "I have been working the soil in my garden.",
+                "The earth is rich!",
+                "We will be able to grow many things here.",
+                "Insha'Allah it will grow!",
+                "We should look at importing some seedlings.",
+                "There is much to do, much to grow."
+            ],
+            [
+                "Look at all this green...",
+                "It is amazing.",
+                "There has never been green like this before.",
+                "La hawla wa la, quwwata illa bilah.",
+                "Every day I plant one more tree.",
+                "Imagine how far we could stretch this forest.",
+                "We could turn the whole desert green."
+            ]
+        ],
+        HOUSE1: [
+            [
+                "Great Salaams on you Professor.",
+                "We are well.",
+                "The house is amazing.",
+                "We have a vegetable garden now.",
+                "Come and visit us when you are free.",
+                "There are chickens now!",
+                "We will wait for you."
+            ],
+            [
+                "This land has been blessed.",
+                "We have been blessed.",
+                "You are like a blessing for us Professor.",
+                "Subhan'Allahi wa bi hamdi!",
+                "Life has never been so easy.",
+                "We do not even work the fields now.",
+                "How can we ever want for more?"
+            ],
+            [
+                "La ilaha ila Allah! Always!",
+                "We were destitute, now we are affluent.",
+                "Is it not amazing how things do change?",
+                "Professor, you have worked hard in this land.",
+                "May you be blessed!",
+                "We lift our hands only to pray now.",
+                "Beautiful, beautiful place!"
+            ]
+        ],
+        HOUSE2: [
+            [
+                "Hahaha, ah Professor, there you are!",
+                "Come, let me show you something special.",
+                "Ha! Look at us now, so rich.",
+                "From bedoins to aristocracy.",
+                "May our money never end, never stop, never decrease!",
+                "Come Professor, let us enjoy this wealth!",
+                "Aha! I have gold plates for your vehicle! Where are they now?"
+            ],
+            [
+                "Ah, what a sore sight for dignified eyes..",
+                "What is the matter with your Kins, they are so unkempt.",
+                "Dirty creatures, like what they came from.",
+                "They are like pigs, before and after.",
+                "We need servants here, proper servants, clean and capable.",
+                "We cannot forever walk on dusty streets and wash our own cloth.",
+                "We are kings and queens. We should live as such."
+            ],
+            [
+                "Dirty Kins. They are like pack animals now. Ha!",
+                "I spit on them where I see them, prod them, scold them, haha.",
+                "Too stupid to know anything other than following.",
+                "Look at how they looked down on us. See us now! Affluent. Superior.",
+                "Never will we return to the sands and dust of yesteryear.",
+                "We will be better than all in the land.",
+                "We need no other filth coming here. This land is ours alone."
+            ]
+        ]
+    }
 }
 export default Dialogue;
--- src/spawner/CivilianSpawner.js
+++ src/spawner/CivilianSpawner.js
@@ -1,17 +1,24 @@
 import WorldConsts from "consts/WorldConsts";
 import SpriteBuilder from "components/SpriteBuilder";
 import SpritePhysics from "components/SpritePhysics";
+import Interaction from "../components/Interaction";
+import Interactions from "../consts/Interactions";
+import Chat from '../classes/Chat';
+import Dialogue from "../consts/Dialogue";
+import Buildings from "../consts/Buildings";
 
 class CivilianSpawner {
     
     constructor(scene) {
         this.scene = scene;
+        this.setupDialogue();
     }
 
-    spawnCivilian(building) {
+    spawnCivilian(house) {
 
         let civ = SpriteBuilder.GetCivilianSprite();
-        civ.setPosition(building.x, WorldConsts.GROUND_Y - 16);
+        civ.setPosition(house.x, WorldConsts.GROUND_Y - 16);
+        civ.isMale = Math.random() > .5;
 
         this.scene.addSpriteToSceneAndGroups(
             civ,
@@ -20,7 +27,88 @@ class CivilianSpawner {
         );
         SpritePhysics.AddPhysics(civ);
 
-        civ.setHome(building);
+        civ.setHome(house);
+
+        let dialogues = this.chats.get(house.getType());
+        if (dialogues && dialogues.length > 0) {
+
+            let name = civ.isMale ? this.getMaleName() : this.getFemaleName();
+            let list = dialogues.shift();
+            let className = Dialogue.TYPE_CIV;
+            let chat = new Chat(name, list, className);
+            Interaction.AddInteraction(civ, Interactions.POST, chat);
+
+            this.scene.addSpriteToSceneAndGroups(
+                civ,
+                this.scene.talkingGroup
+            );
+        }
+    }
+
+    setupDialogue() {
+
+        let fileChats = new Map();
+        this.assignDialogueToBuildings(fileChats);
+        
+        let chatCopy = new Map(JSON.parse(JSON.stringify(Array.from(fileChats))));
+        this.chats = chatCopy;
+    }
+
+    assignDialogueToBuildings(map) {
+
+        map.set(Buildings.LAB_TABLE, Dialogue.HUT);
+        map.set(Buildings.PLAYER_HOUSE, Dialogue.HUT);
+
+        map.set(Buildings.TENT1, Dialogue.NPC.TENT1);
+        map.set(Buildings.TENT2, Dialogue.NPC.TENT2);
+        map.set(Buildings.TENT3, Dialogue.NPC.TENT3);
+
+        map.set(Buildings.HUT, Dialogue.HUT);
+        map.set(Buildings.FARMHUT, Dialogue.HUT);
+        map.set(Buildings.HOUSE1, Dialogue.HOUSE1);
+        map.set(Buildings.HOUSE2, Dialogue.HOUSE2);
+
+        map.set(Buildings.MOSQUE, Dialogue.HUT);
+        
+        map.set(Buildings.PUB, Dialogue.HUT);
+        map.set(Buildings.SHOP, Dialogue.HUT);
+        map.set(Buildings.STORE, Dialogue.HUT);
+        map.set(Buildings.RESTAURANT, Dialogue.HUT);
+
+        map.set(Buildings.RANCH, Dialogue.HUT);
+        map.set(Buildings.WINDMILL, Dialogue.HUT);
+    }
+
+    getMaleName(isFemale = false) {
+
+        const c1 = 'ADFGKMPQTVZ';
+        const c2 = 'AEIOU';
+        const c3 = 'GHJNESVXYZ';
+
+        let l1 = Phaser.Utils.Array.GetRandom(c1.split(""));
+        let l2 = Phaser.Utils.Array.GetRandom(c2.split(""));
+        let l3 = Phaser.Utils.Array.GetRandom(c2.split(""));
+        let l4 = Phaser.Utils.Array.GetRandom(c3.split(""));
+
+        let name = l2 + l1 + l3 + l4 + l4;
+        return name;
+    }
+
+    getFemaleName() {
+
+        const c1 = 'ADFKPQVZ';
+        const c2 = 'AEIOU';
+        const c3 = 'BCHJLNRSTVWXYZ';
+        const femEnds = "A-AH-YA-OA-IA"
+
+        let l1 = Phaser.Utils.Array.GetRandom(c1.split(""));
+        let l2 = Phaser.Utils.Array.GetRandom(c2.split(""));
+        let l3 = Phaser.Utils.Array.GetRandom(c2.split(""));
+        let l4 = Phaser.Utils.Array.GetRandom(c3.split(""));
+        let l5 = Phaser.Utils.Array.GetRandom(femEnds.split("-"));
+
+        let name = l1 + l2 + l3 + l4 + l5;
+        return name;
     }
 }
 export default CivilianSpawner;
